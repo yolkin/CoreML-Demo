831367bc86646689dfe1c6bb4665ae7cd3b2acdb
Alexander <m2t.alexander@gmail.com>
2017-06-15 16:03:17 +0300

    Working App

---
+++ CoreML Demo (Machine Learning)/UIImageExtension.swift
@@ -0,0 +1,51 @@
+//
+//  PixelBufferExtension.swift
+//  CoreML Demo (Machine Learning)
+//
+//  Created by Alexander on 15.06.17.
+//  Copyright © 2017 Alexander. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    func pixelBuffer() -> CVPixelBuffer? {
+        
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(self.size.width), Int(self.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        
+        guard (status == kCVReturnSuccess) else {
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(self.size.width), height: Int(self.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        
+        context?.translateBy(x: 0, y: self.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return pixelBuffer
+    }
+    
+    func resizeTo224px() -> UIImage? {
+        
+        UIGraphicsBeginImageContext(CGSize(width: 224, height: 224))
+        self.draw(in: CGRect(x: 0, y: 0, width: 224, height: 224))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return resizedImage
+    }
+    
+}
---
+++ CoreML Demo (Machine Learning)/ViewController.swift
@@ -0,0 +1,97 @@
+//
+//  ViewController.swift
+//  CoreML Demo (Machine Learning)
+//
+//  Created by Alexander on 15.06.17.
+//  Copyright © 2017 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var probsLabel: UILabel!
+    @IBOutlet weak var choosePicLabel: UILabel!
+    @IBOutlet weak var addImageButton: UIButton!
+    
+    let imagePicker = UIImagePickerController()
+    
+    let model = Resnet50()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        probsLabel.text  = ""
+        resultLabel.text = ""
+        resultLabel.addGradient()
+        
+        imagePicker.delegate   = self
+        imagePicker.sourceType = .photoLibrary
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addImage(_ sender: Any) {
+        addImageButton.setTitle("", for: .normal)
+        choosePicLabel.isHidden = true
+        
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func recognize(_ sender: Any) {
+        resultLabel.isHidden = false
+        
+        let resizedImage = imageView.image?.resizeTo224px()
+        
+        guard let pxlBufImage = resizedImage?.pixelBuffer() else {
+            return
+        }
+        
+        do {
+            let prediction = try model.prediction(image: pxlBufImage)
+            
+            let sortedPrediction = prediction.classLabelProbs.sorted(by: { (lhs, rhs) -> Bool in
+                return lhs.value > rhs.value
+            })
+            
+            resultLabel.text = prediction.classLabel
+            probsLabel.text = """
+            \(sortedPrediction[0].key): \(NSString(format: "%.2f", sortedPrediction[0].value))
+            \(sortedPrediction[1].key): \(NSString(format: "%.2f", sortedPrediction[1].value))
+            \(sortedPrediction[2].key): \(NSString(format: "%.2f", sortedPrediction[2].value))
+            """
+            
+        } catch {
+            print(error)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        imageView.image = image
+        
+        resultLabel.text = ""
+        probsLabel.text = ""
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension UILabel {
+    
+    func addGradient() {
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor(red: 0, green: 0, blue: 0, alpha: 0).cgColor, UIColor(red: 0, green: 0, blue: 0, alpha: 0.34).cgColor]
+        gradientLayer.locations = [0.0, 0.7]
+        gradientLayer.frame = CGRect(x: 0.0, y: 0.0, width: self.frame.size.width, height: self.frame.size.height)
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+}
+
